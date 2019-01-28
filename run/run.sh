30e3348c0fe18a63ff6e15ff8fb4ec915c1726dc
Ilya Medyanikov <ilya.medyanikov@gmail.com>
2019-01-29 01:38:18 +0300

    Refactor run.sh

--- run/run.sh
+++ run/run.sh
@@ -1,29 +1,27 @@
+#!/usr/bin/env bash
+
+MASTER_PORT=11000
+SLAVES_PORT=11001
+SLAVES_COUNT=5
+
 function get_slaves() {
-    RES="http://localhost:11001"
-    for i in {2..9}
+    for i in $(seq -w 1 ${SLAVES_COUNT})
     do
-        RES=$RES",http://localhost:1100$i"
-    done
-    for i in {10..20}
-    do
-        RES=$RES",http://localhost:110$i"
+        RES=$RES",http://localhost:$((${SLAVES_PORT} + $i - 1))"
     done
     echo $RES
 }
 
 SLAVES=$(get_slaves)
+SLAVES=${SLAVES:1}
 
 SUCCESS=0
 go install -i ../client && go install -i ../master && go install -i ../slave && SUCCESS=1
 if [[ $SUCCESS == 1 ]]
 then
-    $(go env GOPATH)/bin/master start -name "master" -port "11000" -override -slaves ${SLAVES} &
-    for i in {1..9}
+    $(go env GOPATH)/bin/master start -name "master" -port "${MASTER_PORT}" -override -slaves ${SLAVES} &
+    for i in $(seq -w 1 ${SLAVES_COUNT})
     do
-        $(go env GOPATH)/bin/slave start -name "slave$i" -port "1100$i" -master http://localhost:11000 -override &
-    done
-    for i in {10..20}
-    do
-        $(go env GOPATH)/bin/slave start -name "slave$i" -port "110$i" -master http://localhost:11000 -override &
+        $(go env GOPATH)/bin/slave start -name "slave$i" -port "$((${SLAVES_PORT} + $i - 1))" -master "http://localhost:${MASTER_PORT}" -override &
     done
 fi
