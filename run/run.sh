328a064333b4b5ecec7d6366caced2e990941e57
Ilya Medyanikov <ilya.medyanikov@gmail.com>
2019-05-19 08:16:27 +0300

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -3,6 +3,9 @@
 MASTER_PORT=11000
 SLAVES_PORT=11001
 SLAVES_COUNT=5
+MASTER_ADDR="http://localhost:${MASTER_PORT}"
+SCHEDULER_PORT=$((${SLAVES_PORT} + $SLAVES_COUNT))
+SCHEDULER_ADDR="http://localhost:${SCHEDULER_PORT}"
 
 function get_slaves() {
     for i in $(seq -w 1 ${SLAVES_COUNT})
@@ -19,9 +22,14 @@ SUCCESS=0
 go install -i ../client && go install -i ../master && go install -i ../slave && SUCCESS=1
 if [[ $SUCCESS == 1 ]]
 then
-    $(go env GOPATH)/bin/master start -name "master" -port "${MASTER_PORT}" -override -slaves ${SLAVES} &
+    $(go env GOPATH)/bin/master start -name "master" -port $MASTER_PORT -override -slaves $SLAVES -schedulers $SCHEDULER_ADDR &
     for i in $(seq -w 1 ${SLAVES_COUNT})
     do
-        $(go env GOPATH)/bin/slave start -name "slave$i" -port "$((${SLAVES_PORT} + $i - 1))" -master "http://localhost:${MASTER_PORT}" -override &
+        rm -rf "$MR_PATH/slave$i/sources"
+        mkdir "$MR_PATH/slave$i/sources"
+        cp ../template/build.sh "$MR_PATH/slave$i/sources/"
+        cp -r ../template/main "$MR_PATH/slave$i/sources/main"
+        $(go env GOPATH)/bin/slave start -name "slave$i" -port "$((${SLAVES_PORT} + $i - 1))" -master $MASTER_ADDR -override &
     done
+    $(go env GOPATH)/bin/slave start -name "scheduler" -port $SCHEDULER_PORT -master $MASTER_ADDR -scheduler -override &
 fi
