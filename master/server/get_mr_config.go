573acae26808c14f4026a79ad4339491e90ca1fb
Ilya Medyanikov <ilya.medyanikov@gmail.com>
2019-12-13 06:32:27 +0300

    Change block size

--- master/server/get_mr_config.go
+++ master/server/get_mr_config.go
@@ -3,5 +3,5 @@ package server
 import "github.com/Croohand/mapreduce/common/responses"
 
 func getMrConfig() *responses.MrConfig {
-	return &responses.MrConfig{MaxRowLength: 1 << 15, BlockSize: 1 << 25, ReplicationFactor: 3}
+	return &responses.MrConfig{MaxRowLength: 1 << 15, BlockSize: 1 << 20, ReplicationFactor: 3}
 }
